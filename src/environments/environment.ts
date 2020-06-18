b0d8ae84aced643a6d10fd67b058a835715543ac
YoselynCastillo <yose.castillo06@gmail.com>
2020-06-18 10:32:36 -0400

    Configuracion exitosa de Firebase, Bootstrap y Fontawesome

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBrer0ixdMbd9-uzvUnTCVL67BY6jNU1eY",
+    authDomain: "fireapp-b9b54.firebaseapp.com",
+    databaseURL: "https://fireapp-b9b54.firebaseio.com",
+    projectId: "fireapp-b9b54",
+    storageBucket: "fireapp-b9b54.appspot.com",
+    messagingSenderId: "985072936776",
+    appId: "1:985072936776:web:b52603012ae57eea8b757d",
+    measurementId: "G-WZ31Z75G3F"
+  },
+};
